37458be43e74cae0f078b237a3db4e31857209fd
OmidRashidiFam <omidRashidiFam@gmail.com>
2023-02-18 00:26:54 +0330

    add navbar and linking with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./routes/navbar/navbar";
 import HomePage from './routes/home-page/home-page'
+import ShopPage from './routes/shop-page/shop-page'
+import ContactPage from './routes/contact-page/contact-page'
+import SignupLoginPage from './routes/signup-login-page/signup-login-page'
 import './App.css'
 
 function App() {
   return (
     <div className='app'>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<HomePage />} />
+          <Route path="shop" element={<ShopPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="signup-login" element={<SignupLoginPage />} />
+        </Route>
       </Routes>
     </div>
-
   );
 }
 
---
+++ src/routes/signup-login-page/signup-login-page.jsx
@@ -0,0 +1,7 @@
+const SignupLoginPage = () => {
+  return (
+    <h1>signup login page</h1>
+  );
+}
+
+export default SignupLoginPage;
