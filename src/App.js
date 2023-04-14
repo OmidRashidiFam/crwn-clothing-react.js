cf5a7a2355bd76e04e5f29a9634589217246616f
OmidRashidiFam <omidRashidiFam@gmail.com>
2023-04-14 22:22:36 +0330

    fixt some issue in path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./routes/navbar/navbar";
 import HomePage from "./routes/home-page/home-page";
-import ShopPage from "./routes/shop-page/categories-preview-page/categories-preview-page";
+import ShopPage from "./routes/shop-page/shop-page";
 import ContactPage from "./routes/contact-page/contact-page";
 import AuthpPage from "./routes/auth-page/auth-page";
 import CheckoutPage from "./routes/checkout-page/checkout-page";
