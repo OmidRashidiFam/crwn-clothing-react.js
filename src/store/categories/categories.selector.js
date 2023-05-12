c8109792db7062aecd72fdef16db208ef6f5e727
OmidRashidiFam <omidRashidiFam@gmail.com>
2023-05-12 11:50:16 +0330

    Refactor selectCategoriesMap using createSelector from reselect library

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,7 +1,19 @@
-export const selectCategoriesMap = (state) => {
-  return state.categories.categoriesArr.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-};
+import { createSelector } from "reselect";
+
+const selectCategoriesReducer = (state) => state.categories;
+
+const selectCategoriesArr = createSelector(
+  [selectCategoriesReducer],
+  (categoriesSlice) => categoriesSlice.categoriesArr
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategoriesArr],
+  (categoriesArr) => {
+    return categoriesArr.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
