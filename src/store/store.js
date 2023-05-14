c6f9d7a49cf6a7f52d2ec86115928f542b552ed0
OmidRashidiFam <omidRashidiFam@gmail.com>
2023-05-14 08:02:35 +0330

    add some meaningful comments

--- src/store/store.js
+++ src/store/store.js
@@ -3,18 +3,22 @@ import { compose, createStore, applyMiddleware } from "redux";
 // import the root reducer
 import { rootReducer } from "./root-reducer";
 
-// import the logger from  redux-logger
+// create a customLogger
 const customLogger = (store) => (next) => (action) => {
+  // if no action type is present, return the next action
   if (!action.type) {
     return next(action);
   }
 
+  // log action type, payload and previous state
   console.log("action type: ", action.type);
   console.log("action payload: ", action.payload);
   console.log("prev state: ", store.getState());
 
+  // call the next action
   next(action);
 
+  // log the next state
   console.log("next state: ", store.getState());
 };
 
