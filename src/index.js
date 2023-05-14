cbfb5c642ca254e652485008db5ab80fc6aad3bf
OmidRashidiFam <omidRashidiFam@gmail.com>
2023-05-14 09:37:05 +0330

    apply redux persist library

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./store/store";
 
 import App from "./App";
 import { store } from "./store/store";
@@ -10,9 +12,11 @@ import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
+  </Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
-// import the necessary redux functions
+// import the necessary redux functions and methods
 import { compose, createStore, applyMiddleware } from "redux";
+// import the necessary redux-persist functions
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // import the root reducer
 import { rootReducer } from "./root-reducer";
 
@@ -9,18 +12,25 @@ const customLogger = (store) => (next) => (action) => {
   if (!action.type) {
     return next(action);
   }
-
   // log action type, payload and previous state
   console.log("action type: ", action.type);
   console.log("action payload: ", action.payload);
   console.log("prev state: ", store.getState());
-
   // call the next action
   next(action);
-
   // log the next state
   console.log("next state: ", store.getState());
 };
+
+// create a config object for persist
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+// create a persisted reducer using persisitreducer
+const persistedreducer = persistReducer(persistConfig, rootReducer);
 
 // define middlewares to be used
 const middleWares = [customLogger];
@@ -28,5 +38,12 @@ const middleWares = [customLogger];
 // compose enhancers for store creation
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-// create store with root reducer, no initial state and composed enhancers
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+// create store with persistedreducer, no initial state and composed enhancers
+export const store = createStore(
+  persistedreducer,
+  undefined,
+  composedEnhancers
+);
+
+// create a persistor using persistStore
+export const persistor = persistStore(store);
